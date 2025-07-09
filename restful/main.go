cc4a21198e31c77e9a59aa860ea185d5d52daf4a
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2025-07-09 08:48:30 +0800

    restapi gin

---
+++ restful/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Artist string `json:"artist"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "T1", Artist: "A1"},
+	{ID: "2", Title: "T2", Artist: "A2"},
+	{ID: "3", Title: "T3", Artist: "A3"},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func getAlbumsByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumsByID)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
